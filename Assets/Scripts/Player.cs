b3f2d5fb829bdc5be26b8bf8506394bc97c14b9d
tiiedye <tiiedye@gmail.com>
2020-11-24 13:05:14 -0800

    Adding to Script
    Adding some basic movement of Player in script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    // public || private reference, data type, variable name, optional value
+    // if private add _ to start of variable name, to easily distinguish private variables at a glance.
+    // serialize field attribute allows developer to read & modify private variables in the unity inspector, but other game objects
+        // and scripts cannot touch it.
+    [SerializeField]
+    private float _speed = 3.5f;
+
     void Start()
     {
         // take the current position = new position (0, 0, 0)
@@ -14,6 +22,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.right * _speed * Time.deltaTime);
     }
 }
