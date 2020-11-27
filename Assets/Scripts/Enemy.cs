81e3028bf30b3528ee9900ec700f5c0edbb3d749
tiiedye <tiiedye@gmail.com>
2020-11-27 13:05:06 -0800

    Enemy Movement
    Programming Enemy movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _enemySpeed = 4.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Moves Enemy sprite down.
+        transform.Translate(Vector3.down * _enemySpeed * Time.deltaTime);
+
+        // if Enemy reaches the bottom of the screen, it re-spawns at the top of the screen at a new random position.
+        if (transform.position.y < -5f) {
+            float randomX = Random.Range(-8f, 8f);
+            transform.position = new Vector3(randomX, 7f, 0);
+        }
+    }
+}
