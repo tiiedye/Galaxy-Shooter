1459033e3386d2021fb78687afde87500765fc81
tiiedye <tiiedye@gmail.com>
2020-11-27 11:55:19 -0800

    Laser Velocity
    Adding movement to the laser so that it moves "up" when spawned.

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // speed variable of 8
+    [SerializeField]
+    private float _laserVelocity = 8.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // translate laser up
+        transform.Translate(Vector3.up * _laserVelocity * Time.deltaTime);
+    }
+}
