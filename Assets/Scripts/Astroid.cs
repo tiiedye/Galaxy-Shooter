55a6e52fb728e83b4c6c8e18d016b2b9ed952067
tiiedye <tiiedye@gmail.com>
2020-12-06 10:05:47 -0800

    Setting up Enemy Explosion and Asteroid
    Setting up the Enemy explosion on death and adding an asteroid to start the level

---
+++ Assets/Scripts/Astroid.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Astroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotateSpeed = 19.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,10 +7,21 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private float _enemySpeed = 4.0f;
     private Player _player;
+    private Animator _anim;
 
     void Start()
     {
-        _player = GameObject.Find("Player").GetComponent<Player>();    
+        _player = GameObject.Find("Player").GetComponent<Player>();  
+        
+        if (_player == null) {
+            Debug.LogError("Player is null");
+        }
+
+        _anim = GetComponent<Animator>();
+
+        if (_anim == null) {
+            Debug.LogError("Animator is null");
+        }
     }
 
     // Update is called once per frame
@@ -37,7 +48,9 @@ public class Enemy : MonoBehaviour
                 player.Damage();
             }
 
-            Destroy(this.gameObject);
+            _anim.SetTrigger("OnEnemyDeath");
+            _enemySpeed = 0;
+            Destroy(this.gameObject, 2.5f);
         }
 
         // Destroys Enemy & Laser if they collide, and updates score.
@@ -48,7 +61,9 @@ public class Enemy : MonoBehaviour
                 _player.AddScore();
             }
 
-            Destroy(this.gameObject);
+            _anim.SetTrigger("OnEnemyDeath");
+            _enemySpeed = 0;
+            Destroy(this.gameObject, 2.5f);
         }
     }
 }
