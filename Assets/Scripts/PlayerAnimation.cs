20daefec2e8ea30d72702fbce9098ded1b95017a
tiiedye <tiiedye@gmail.com>
2020-12-21 11:21:09 -0800

    adding Player Animations
    Adding Player turn animations on A down and Up, and D down and Up

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _anim;
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            _anim.SetBool("Turn_Left", true);
+            _anim.SetBool("Turn_Right", false);
+        }
+        
+        if (Input.GetKeyUp(KeyCode.A)) {
+            _anim.SetBool("Turn_Left", false);
+            _anim.SetBool("Turn_Right", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)) {
+            _anim.SetBool("Turn_Left", false);
+            _anim.SetBool("Turn_Right", true);
+        } else if (Input.GetKeyUp(KeyCode.D)) {
+            _anim.SetBool("Turn_Left", false);
+            _anim.SetBool("Turn_Right", false);
+        }
+    }
+}
